e0a588f2c60e8ddbc4e37650a6e43e5262ba7db2
yanscarneiro <yan.cpm@gmail.com>
2019-04-09 12:42:45 -0300

    Update SIPEEv1.1.py
    Correção de dados

--- SIPEEv1.1.py
+++ SIPEEv1.1.py
@@ -24,11 +24,11 @@ if raio_a >= 440:
         t2 = t - 10
         raio_b1=raio_b
         while True:
-            ic_n2 = 6*(raio_b1)^0.5
+            ic_n2 = 6*(raio_b1)**0.5
             q_n2 = ic_n2/2
             raio_b2 = (t2-q_n2)/tan(ac_b/2)
             if raio_b1 == raio_b2:
-                pb = (ic_n2^2)/24*raio_b2
+                pb = (ic_n2**2)/24*raio_b2
                 raio_b = raio_b2 - pb
                 break
             elif raio_b1 != raio_b2:
@@ -41,25 +41,27 @@ if raio_a >= 440:
 else:
     #3º hipotese CCT x CCT
     t3 = d/2
+    raio_a = t3/tan(ac_a/2)
     raio_a1=raio_a
     while True:
-        ic_n1 = 6*(raio_a1)^0.5
+        ic_n1 = 6*(raio_a1)**0.5
         q_n1 = ic_n1/2
         raio_a2 = (t3-q_n1)/tan(ac_a/2)
         if raio_a1 == raio_a2:
-            pa = (ic_n1^2)/24*raio_a2
+            pa = (ic_n1**2)/24*raio_a2
             raio_a = raio_a2 - pa
             break
         elif raio_a1 != raio_a2:
             raio_a1 = raio_a2
             continue
+    raio_b = t3/tan(ac_b/2)
     raio_b1=raio_b
     while True:
-        ic_n2 = 6*(raio_b1)^0.5
+        ic_n2 = 6*(raio_b1)**0.5
         q_n2 = ic_n2/2
         raio_b2 = (t3-q_n2)/tan(ac_b/2)
         if raio_b1 == raio_b2:
-            pb = (ic_n2^2)/24*raio_b2
+            pb = (ic_n2**2)/24*raio_b2
             raio_b = raio_b2 - pb
             break
         elif raio_b1 != raio_b2:
